69b0522f14e483d3ea867f09b9c7a0cf21f64188
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-06-12 19:31:12 +0300

    main

--- main.cpp
+++ main.cpp
@@ -32,6 +32,34 @@ int main() {
 
     int menu = 0;
 
+#if CREATE_OR_LOAD == 1
+    sizeBank = 3;
+    bank = new Card[sizeBank] {
+        {"Ivanov Volodymyr", 12345, 250},
+        {"Kalashnikov Marko", 67895, 500},
+        {"Konovalenko Tymofii", 45678, 300}
+    };
+#elif CREATE_OR_LOAD == 0
+    loadArrayFromBinFile(bank, sizeBank);
+#endif
+
+
+    do {
+        cout << "===========================================\n";
+        cout << "\t MENU:\n";
+        cout << "===========================================\n";
+        cout << "0 - Save and Exit\n";
+        cout << "1 - Show all cards.\n";
+        cout << "===========================================\n";
+        cout << ">>> ";
+        cin >> menu;
+
+        switch (menu) {
+            case 0: saveArrayToBinFile(bank, sizeBank); break;
+            case 1: showArrayCards(bank, sizeBank); break;
+        }
+    } while (menu != 0);
+
 
 
     return 0;
