1aec9341024e0b34d3e66900c5007ebb48d8599a
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-06-12 18:28:36 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include "Card.h"
 
 
 int main() {
 
+    Card Bohdan;
+    Bohdan.createCard();
+
+    cout << "--------------------------\n";
+    Bohdan.showCard();
+
+
+    FILE* file = nullptr;
+    //fopen_s(&file, "bank.bin", "w"); //VS
+    file = fopen("bank.bin", "w"); //CLion, Online
+    if (file!= nullptr) {
+        Bohdan.saveCardToBinFile(file);
+        fclose(file);
+        cout << "Saved!\n";
+    }
 
     return 0;
 }
